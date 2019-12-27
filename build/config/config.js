81ec74500113453a2ef62da1ca8be927d71120b0
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2019-12-27 13:47:59 +0200

    webpack

---
+++ build/config/config.js
@@ -0,0 +1,16 @@
+export default {
+
+    app: {
+        name: "ZERO",
+        version: "0.1",
+        description: "DESCRIPTION",
+        logo: "/public/assets/images/logo-image",
+        image: "/public/assets/images/cover-image",
+        keywords: ["decentralized","zero"],
+    },
+
+    consensus: [{
+        address: "http://localhost:4000",
+    }]
+
+}
