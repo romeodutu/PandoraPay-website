43f9b6effaa92fc6002283c83eea2c160a99c54f
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2020-02-02 02:49:10 +0200

    i18n more text

--- src/util/languages/langs/en.js
+++ src/util/languages/langs/en.js
@@ -7,6 +7,14 @@ export default {
     homepage:{
         title: "World's First Anonymous Exchange",
         subTitle: "Anonymous payments made easy for average Joe",
+        blocks: 'Blocks',
+        transactions: 'Transactions',
+        uniqueAccounts: 'Unique Accounts',
+        realTimeDataFrom: 'Real time data from',
+        testNetBlockchainExplorer: 'Test net blockchain explorer',
+        anonymousCashForTheInternet: 'Anonymous cash for the internet',
+        worldsFirstAnonymousDecentralizedExchange: "World's First Anonymous Decentralized Exchange",
+        itsFastPrivateAndSecure: "It's fast, private and secure. With PandoraPay, you are your own bank. You can send money totally anonymously to anyone knowing that others cannot see your balance or track your activity.",
     }
 
 }
