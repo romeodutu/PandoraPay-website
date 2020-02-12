bfd88329df6b860d3c0f8cc83e04511dc33a8c9f
romeodutu <48035798+romeodutu@users.noreply.github.com>
2020-02-12 19:06:18 +0200

    Update RO

--- src/util/languages/langs/ro.js
+++ src/util/languages/langs/ro.js
@@ -14,33 +14,179 @@ export default {
         webWallet: "Portofel Digital",
     },
 
-    pages: {
-        homepage: {
-            title: "Primul Exchange Descentralizat Anonim",
-            subTitle: "Plati Anonime facute simplu",
+    pages:{
+
+        homepage:{
+            title: "Primul Exchange Descentralizat Anonim din lume",
+            subTitle: "Plati anonime care pot fi efectuate cu usurinta de oricine",
+            blocks: 'Blocuri',
+            transactions: 'Tranzactii',
+            uniqueAccounts: 'Counturi unice',
+            realTimeDataFrom: 'Timp real de la',
+            testNetBlockchainExplorer: 'Test net blockchain explorer',
+
+            anonymousCashForTheInternet: 'Plati anonime pentru internet',
+            worldsFirstAnonymousDecentralizedExchange: "Primul Exchange Descentralizat din lume",
+            itsFastPrivateAndSecure: `Este rapid, privat si securizat. Cu ${config.app.name}, propria banca. Poti trimite bani total anonim si fara ca cineva sa-ti poata urmari balanta sau tranzactiile din activitate.`,
+
+            getPaidAnonymously: 'Plateste anonim',
+            itsUntraceable: "Fara urme",
+            sendingReceivingAddressesObfuscated: `Trimite si primeste adrese cat si sume intr-un mod ascuns implicit. Tranzactiile din ${config.app.name} nu pot fi legate de un utilizator in particular sau de o identitate reala.`,
+
+            privateDigitalCurrency: "Moneda Digitala Privata",
+            withASingleClick: "cu un singur click",
+            neverBeenEasierToSendAndReceiveMoney: "Niciodata nu a fost mai simplu sa trimiti sau sa primesti bani international in totalitate anonim si fara urme.",
+
+            youAreYourOwnBank: `Esti propria banca`,
+            withNameYouAreYourOwnBank: `Cu ${config.app.name}, esti propria banca iar altii nu-ti pot vedea balanta sau sa-ti urmareasca activitatea. Spune buna ${config.app.name}.`,
+            nameIsSecure: `${config.app.name} este securizat`,
+            unstoppableByUsingDecentralizedBlockchain: `De neoprit utilizand o tehnologie blockchain descentralizata securizata de o retea de utilizatori.`,
+            nameIsPrivate: `${config.app.name} este privat`,
+            youOwnYourOwnKey: `Tu detii propria cheie si nimeni nu te poate depista.`,
+
+            anonymousPaymentsMadeEasy: `Plati anonime realizate cu usurinta.`,
+            webDWallet:`Portofel Web`,
+
+            makeItPerfect:`Perfect pentru dumneavoastra!`,
+            worldFirstAnonymousStablecoin:`Prima stablecoin descentralizata si nedetectabila`,
+
+            descriptionOfCharacterPrivate: `Privat`,
+            descriptionOfCharacterPrivateZether: `Noul mecanism criptografic Zether care utilizeaza Σ-bullets care ascunde originea, suma si destinatia tuturor tranzactiilor.`,
+
+            descriptionOfCharacterUntraceable: `Nedetectabil`,
+            descriptionOfCharacterObfuscated: `Adresele de trimitere și primire, inclusiv sumele tranzacționate, sunt mascate în mod implicit.`,
+
+            descriptionOfCharacterFungible: `Fungibil`,
+            descriptionOfCharacterNoBlacklist: `Unitatile ${config.app.name} nu pot fi trecute pe lista neagra de catre vanzatori sau exchangeuri pentru ca au fost asociate in alte tranzactii.`,
+
+            descriptionOfCharacterAssetPlusCurrency: `Bunuri & Monede`,
+            descriptionOfCharacterAssetPlusCurrencyAnchor: `Anonymous digital assets & currencies that anchor the value of other assets backed by the community.`,
+
+            descriptionOfCharacterConfidentialBlindedEscrow: `Confidential Blinded Escrow`,
+            descriptionOfCharacterCompletelyBlindEscrow: `Escrows don't know the amount, the real sender and the real destination. Escrows are completely blinded.`,
+
+            descriptionOfCharacterReviewsForTraders: `Recenzii pentru comercianti`,
+            descriptionOfCharacterReviewsForTradersAnonymous: `To increase the trust between traders in an untrusted network, anonymous reviews can be written down.`,
+
+            descriptionOfCharacterEncryptedChat: `Chat criptat`,
+            descriptionOfCharacterEncryptedChatEndToEnd: `The chat in ${config.app.name}  between traders and blinded escrow are encrypted end to end.`,
+
+            descriptionOfCharacterNoDeposit: `Fara depozitare`,
+            descriptionOfCharacterNoDepositNoCoins: `${config.app.name} is the first decentralized p2p exchange that allows users to start trading without needing to deposit or owning coins. To buy coins or assets, you don't need to own coins`,
+
+            descriptionOfCharacterInstantAnonymity: `Instant anonymity & HD Wallets`,
+            descriptionOfCharacterAdvancedHdWallet: `It just takes seconds to decrypt and view your balances and transactions. More, it has an advanced HD Wallet implemented.`,
+
+            mainNetTimeline: `Timeline for PandoraPay Main Net`,
+            launchTimeline: `Timeline for launching PandoraPay. We don't want to offer any specific date, but most of the code has already been implemented`,
+
+            featuresTimelineDataStructure: `Structura date`,
+            featuresTimelineNetworking: `Retea`,
+            featuresTimelineCryptoAndRingCT: `Crypto & RingCT - Ring Confidential Signatures`,
+            featuresTimelineBlockchainAndConsensus: `Blockchain & Consensus`,
+            featuresTimelineExchangeFiftyPerCent: `Exchange - 50%`,
+            featuresTimelineEncryptedMessages: `Mesaje criptate`,
+            featuresTimelineCompletlyBlindedEscrow: `Completly Blinded Escrow`,
+            featuresTimelineFinalTests: `Teste Finale`,
+
+            gotAQuestion: `Ai o intrebare? Noi avem raspunsul `,
+            gotAQuestionFaq: "FAQ",
         },
 
         contact:{
+            powerToThePeople: `Oameni`,
+            powerToThePeopleMeetTheCommunity: `Intalneste-ne`,
 
+            aFewGoodMen: `Intalneste aceasta mica echipa care face produse marete`,
+            aFewGoodMenAlexandruIonutBudisteanu: `Alexandru Ionut Budisteanu`,
+            aFewGoodMenAbi: `Alexandru Ionut Budisteanu`,
+            aFewGoodMenFounder: `Fondator`,
+            aFewGoodMenFounderAchievements: `Alexandru is a Forbes Europe 30 under 30 nominee in industry category. In 2013, he was nominated by Time Magazine as one of the most influential teens of the world. MIT named an asteroid with his name.`,
+
+            powerToThePeopleContactTheCommunity: `Contact`,
+            powerToThePeopleTalkToTheCommunity: `Sa vorbim`,
+            powerToThePeopleWaysToContact: `Iata metodele prin care ne poti contacta si ne poti intrebare orice`,
+
+            messageReceivedWaitNow: `Vom primi mesajul tau si te vom contacta curand.`,
+            inputYourName: `Nume`,
+            inputYourEmail: `Email`,
+            inputYourMessage: `Mesaj`,
+            hitSendHard: `TRIMITE`,
         },
 
         faq:{
+            weGotAnswers: `Intrebari frecvente`,
+            weGotAllTheAnswers: `Ai o întrebare? Suntem aici pentru a vă răspunde la întrebări!`,
 
-        }
+            questionTypeOfPaymentAccepted: `Ce plati vor fi acceptate?`,
+            answerTypeOfPaymentAccepted: `'Toate platile vor fi acceptate cu exceptia sistemele de plata ce faciliteaza anularea tranzactiilor pacalind astfel vanzatorii. ${config.app.name} nu este limitat, iar utilizatorii își pot defini propriile metode de plata, inclusiv: Fata-in-Fata, Transfer bancar National/International, SEPA, AliPay, WeChat Pay, Revolut, BTC, ETH, WEBD etc. Unele plăți care facilitează rambursarea ușoară vor fi descurajate pentru cumpărătorii cu un nivel scăzut. O astfel de plată de rambursare este PayPal, deoarece este ușor să faceți rambursări susținând că nu ați primit produsele.`,
+
+            questionPeerToPeerExchange: `Cum funcționează schimbul Peer-to-Peer?`,
+            answerPeerToPeerExchange: `Un schimb are loc atunci când un cumpărător achiziționează de la un vânzător câteva monede/token. Vânzătorul va deschide o oferta prin care vinde anumite active (tokens) și cumpărătorul plătește în afara lanțului pentru a achiziționa aceste active (tokens). După finalizarea și confirmarea plății off-chain de la cumpărător, cumpărătorul va primi activele (tokens) de la vânzător.`,
+
+            questionIsBuyerProtected: `Cum este protejat cumpărătorul ${config.app.name} ?`,
+            answerIsBuyerProtected: `Vânzătorul care deține active și dorește să le vândă la schimb va trebui să depună în mod anonim activele într-un cont special de tranzacționare care este încă deținut de acesta. Apoi, vânzătorul stabilește un escrow orb în acest cont comercial pentru supraveghere comercială. Acum, vânzătorul nu poate muta fonduri din contul de tranzacționare fără permisiunea escrow. În cazul unei dispute, cumpărătorul se poate plânge dovedind că a trimis fondurile către vânzător, dar vânzătorul nu trimite în mod voit fondurile.`,
+
+            questionCompletelyBlindedEscrow: `Ce este un escrow complet orb?`,
+            answerCompletelyBlindedEscrow: `Un Escrow complet orb nu va cunoaște suma tranzacționată, datele comerciantului real și adresa comerciantului real. De asemenea, el nu are puterea de a muta fondurile de unul singur, el este folosit doar pentru a se asigura că cumpărătorul nu mișcă fondurile promise unui vânzător.`,
+
+            questionIsTradeChatEncrypted: `Mesajele de chat sunt criptate în schimburi?`,
+            answerIsTradeChatEncrypted: `DA! Toate mesajele de chat sunt criptate peer-to-peer folosind funcții criptografice și anume ECIES - Schema de criptare integrată curbă eliptică.`,
+
+            questionAddressNeedToKnowOnly: `Cumpărătorul, vânzătorul sau escrowul se vor cunoaște?`,
+            answerAddressNeedToKnowOnly: `Nu! Cumpărătorul nu va cunoaște adresa reală a Vânzătorului. Vânzătorul nu va cunoaște adresa reală a Cumpărătorului. Escrow nu va cunoaște adresele reale ale Cumpărătorului sau Vânzătorului sau suma tranzacționată. Cumpărătorul și vânzătorii vor cunoaște doar adresa Escrow.`,
+
+            questionProofOwnerFunds: `De unde știe Cumpărătorul că Vânzătorul are fondurile promise?`,
+            answerProofOwnerFunds: `Dacă au convenit asupra prețului, Cumpărătorul va solicita automat o dovadă de la Vânzător că are suficiente fonduri pentru a face tranzacția. Vânzătorul va genera o dovadă care să arate că deține suficiente pentru tranzactie.`,
+
+            questionCanWeTradeAnything: `Ce bunuri/tokenuri pot fi tranzacționate?`,
+            answerCanWeTradeAnything: `Orice activ din lumea reală poate fi „tokenizat” într-o formă digitală și tranzacționat pe ${config.app.name}. Oricine poate emite active/tokenuri pe ${config.app.name}. Activele/tokenurile pot fi legate de un activ din lumea reală, pot fi colateralizate de un alt activ/cripto sau chiar necoletarizat.`,
+
+            questionRingSignatureUntraceable: `Cum ${config.app.name} activele/tokenurile nu pot fi urmărite?`,
+            answerRingSignatureUntraceable: `${config.app.name} folosește Confidential Ring Signatures (ca Monero) ceea ce înseamnă că sumele sunt ascunse (confidențiale) și o tranzacție folosește desene aleatorii din rețea (o tranzacție de apel).`,
+        },
 
     },
 
     layout:{
 
-
         navbar:{
+            logoAlt: `${config.app.name}`,
 
+            home: `Acasa`,
+            blockExplorer: `Block Explorer`,
+            contact: `Contact`,
+            faq: `FAQ`,
         },
 
 
         footer:{
 
+            organization: "Organizatie",
+            aboutUs: "Despre",
+            contact: "Contact",
+
+            blockchain: "Blockchain",
+            whyUs:`Why ${config.app.name}?`,
+            security: "Securitate",
+            termsAndConditions: "Termeni si conditii",
+
+            community: "Communitate",
+            github: "GitHub",
+            telegram: "Telegram",
+
+            support: "Suport",
+            helpCenter: "Centeru de ajutor",
+            guides: "Ghid",
+            api: "API",
+            faq: "FAQ",
+
+            footer: `© 2020 ${config.app.name}. Este un proiect opensource creat de o comunitate.`
+
         },
 
     }
+
+
+
 }
